f1651931fd1eb9693a08151135c5e62a1a543f79
raycodeing <114753186+raycodeing@users.noreply.github.com>
2025-09-27 22:41:39 -0300

    Add files via upload

---
+++ Trabalho_Ads/farmacia/classe_farmaco.py
@@ -0,0 +1,18 @@
+# classe_farmaco.py
+from enum import Enum
+
+class ClasseFarmaco(Enum):
+    ANALGESICO = "Analgésico"
+    ANTIBIOTICO = "Antibiótico"
+    ANTIINFLAMATORIO = "Anti‐inflamatório"
+    ANTIDEPRESSIVO = "Antidepressivo"
+    ANSIOLITICO = "Ansiolítico"
+    BENZODIAZEPINICO = "Benzodiazepínico"
+    ANTIEMETICO = "Antiemético"
+    HIPOGLICEMIANTE = "Hipoglicemiante"
+    ANTIHIPERTENSIVO = "Antihipertensivo"
+    OPIODE = "Opióde"
+    ANTICOAGULANTE = "Anticoagulante"
+    ESTATINA = "Estatina"
+    BRONCODILADOR = "Broncodilatador"
+    ANTIHISTAMINICO = "Anti‐histamínico"
